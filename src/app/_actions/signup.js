01012a4785c37cbc6411884f42b602457b80a8b3
Lucas Tomaz <lucas.tomaz@devsquad.com>
2025-03-11 05:29:35 -0300

    Store the user from signup form

--- src/app/_actions/signup.js
+++ src/app/_actions/signup.js
@@ -1,6 +1,8 @@
 'use server'
 
 import { z } from 'zod';
+import User from '@/model/User';
+import connect from '@/services/database';
 
 const schema = z.object({
     name: z.string().min(2),
@@ -14,7 +16,7 @@ export async function createUser(prevState, formData) {
 
     if (!validatedFormData.success) {
         const errors = validatedFormData.error.flatten().fieldErrors;
-    
+        
         return {
             form,
             success: false,
@@ -26,7 +28,20 @@ export async function createUser(prevState, formData) {
         };
     }
 
+    await connect();
+    const person = new User({ ...validatedFormData.data });
+    await person.save();
+
     return {
         success: true,
     };
+}
+
+export async function resetFormState() {
+    'use server'
+    return {
+        success: false,
+        form: {},
+        errors: {}
+    }
 }
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,21 +1,38 @@
 'use client'
 
+import { CheckCircleIcon } from '@heroicons/react/20/solid'
 import Modal from '@/components/modal';
 import Input from '@/components/form/input';
 import { createUser } from '@/app/_actions/signup'
-import { useActionState } from 'react';
-
+import { useActionState, useEffect, useTransition, useState } from 'react';
 
 export default function SignUp({ open, setOpen }) {
-    const [state, formAction] = useActionState(createUser, {});
+    const defaultValues = { form: {}, errors: {}, success: false };
+    const [formState, formAction] = useActionState(createUser, {});
+    const [state, setState] = useState(formState)
 
-    console.log(state.form)
+    useEffect(() => {
+        if (state.success) {
+            setTimeout(() => setOpen(false), 2000);
+        }
+    }, [state.success]);
+
+    useEffect(() => {
+        if (open) {
+            setState(defaultValues);
+        }
+    }, [open]);
+
+    useEffect(() => {
+        setState(formState);
+    }, [formState]);
+
     return (
         <Modal open={open} setOpen={setOpen} onSubmit={formAction}>
             <h2 className="mb-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
                 Sign in to your account
             </h2>
-            <form action={formAction} className="space-y-6">
+            {!state.success && (<form action={formAction} className="space-y-6">
                 <Input name="name" defaultValue={state.form?.name} errors={state.errors?.name} placeholder="Your name" />
                 <Input name="email" type="email" defaultValue={state.form?.email} errors={state.errors?.email} placeholder="you@mail.com" />
                 <Input name="phone" defaultValue={state.form?.phone} errors={state.errors?.phone} placeholder="55 5555-5555" />
@@ -29,14 +46,25 @@ export default function SignUp({ open, setOpen }) {
                     </button>
                     <button
                         type="button"
-                        data-autofocus
                         onClick={() => setOpen(false)}
                         className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:col-start-1 sm:mt-0"
                     >
                         Cancel
                     </button>
                 </div>
-            </form>
+            </form>)}
+            {state.success && (
+                <div className="rounded-md bg-green-50 p-4">
+                    <div className="flex">
+                        <div className="shrink-0">
+                        <CheckCircleIcon aria-hidden="true" className="size-5 text-green-400" />
+                        </div>
+                        <div className="ml-3">
+                            <p className="text-sm font-medium text-green-800">Successfully registered!</p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </Modal>
     );
 };
